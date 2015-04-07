ce36b8c1943226f35487d91061e8308fc9caa188
hongjianlin1989 <ttzb1989@yahoo.com>
2015-04-07 17:39:20 -0400

    some commit

---
+++ fitmoo/fitmoo/FitmooHelper.h
@@ -0,0 +1,13 @@
+//
+//  FitmooHelper.h
+//  fitmoo
+//
+//  Created by hongjian lin on 4/7/15.
+//  Copyright (c) 2015 com.fitmoo. All rights reserved.
+//
+
+#ifndef fitmoo_FitmooHelper_h
+#define fitmoo_FitmooHelper_h
+
+
+#endif
---
+++ fitmoo/fitmoo/FitmooHelper.m
@@ -0,0 +1,9 @@
+//
+//  FitmooHelper.m
+//  fitmoo
+//
+//  Created by hongjian lin on 4/7/15.
+//  Copyright (c) 2015 com.fitmoo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
