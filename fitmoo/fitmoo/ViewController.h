c0e7da1b4f2b6aa7178e2bfbdb22dac4832dbc94
hongjianlin1989 <ttzb1989@yahoo.com>
2015-04-07 15:18:13 -0400

    first commit

---
+++ fitmoo/fitmoo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  fitmoo
+//
+//  Created by hongjian lin on 4/7/15.
+//  Copyright (c) 2015 com.fitmoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ fitmoo/fitmoo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  fitmoo
+//
+//  Created by hongjian lin on 4/7/15.
+//  Copyright (c) 2015 com.fitmoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
