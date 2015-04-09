a80a451612d8d253a25a60efd81b2ec0b5a9e8d4
hongjianlin1989 <ttzb1989@yahoo.com>
2015-04-09 18:15:09 -0400

    some commit

---
+++ fitmoo/fitmoo/ShareTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ShareTableViewCell.h
+//  fitmoo
+//
+//  Created by hongjian lin on 4/9/15.
+//  Copyright (c) 2015 com.fitmoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShareTableViewCell : UITableViewCell
+
+@end
---
+++ fitmoo/fitmoo/ShareTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  ShareTableViewCell.m
+//  fitmoo
+//
+//  Created by hongjian lin on 4/9/15.
+//  Copyright (c) 2015 com.fitmoo. All rights reserved.
+//
+
+#import "ShareTableViewCell.h"
+
+@implementation ShareTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
