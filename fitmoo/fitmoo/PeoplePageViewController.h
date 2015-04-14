3ec4cbcc129b15c8f012f0482aa744c669fc9c5e
hongjianlin1989 <ttzb1989@yahoo.com>
2015-04-14 10:57:08 -0400

    before doing fb login

---
+++ fitmoo/fitmoo/PeoplePageViewController.h
@@ -0,0 +1,13 @@
+//
+//  PeoplePageViewController.h
+//  fitmoo
+//
+//  Created by hongjian lin on 4/14/15.
+//  Copyright (c) 2015 com.fitmoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PeoplePageViewController : UIViewController
+
+@end
---
+++ fitmoo/fitmoo/PeoplePageViewController.m
@@ -0,0 +1,37 @@
+//
+//  PeoplePageViewController.m
+//  fitmoo
+//
+//  Created by hongjian lin on 4/14/15.
+//  Copyright (c) 2015 com.fitmoo. All rights reserved.
+//
+
+#import "PeoplePageViewController.h"
+
+@interface PeoplePageViewController ()
+
+@end
+
+@implementation PeoplePageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
