0883a8c154532cdb0fe0cb80d681bf772e6d571d
hongjian lin <hongjianlin@hongjians-MacBook-Pro-2.local>
2016-12-03 19:12:17 -0500

    1. update Facebook SDK, 2. UPDATE Youtube SDK. 3 fix crash when build to real device. 4 update cocoa pod.  5. remove PNG that cause crash.

---
+++ fitmoo/Pods/Target Support Files/Pods-fitmoo/Pods-fitmoo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_fitmoo : NSObject
+@end
+@implementation PodsDummy_Pods_fitmoo
+@end
---
+++ fitmoo/Pods/Target Support Files/Pods-fitmooTests/Pods-fitmooTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_fitmooTests : NSObject
+@end
+@implementation PodsDummy_Pods_fitmooTests
+@end
